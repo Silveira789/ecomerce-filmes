cd295ce3c4e7dba8cae3d686c8b1a53906948c7f
Guilherme Silveira <guilhermesf1210@gmail.com>
2024-05-11 09:26:06 -0300

    Adição model Funcionario

---
+++ src/main/java/br/unitins/topicos1/model/Funcionario.java
@@ -0,0 +1,17 @@
+package br.unitins.topicos1.model;
+
+public class Funcionario  extends DefaultEntity{
+
+    private String nome;
+
+    private String cpf;
+
+    private String email;
+
+    private String telefone;
+
+    private String matricula;
+
+    
+
+}
---
+++ src/main/java/br/unitins/topicos1/model/ItemPedido.java
@@ -0,0 +1,13 @@
+package br.unitins.topicos1.model;
+
+public class ItemPedido extends DefaultEntity{
+
+    private Integer quantidade;
+
+    private Double preco;
+
+    
+
+    // private Pedido Pedido
+
+}
