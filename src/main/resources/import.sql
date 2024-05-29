f21186bece11a38cb035155a796365c3c47e2304
Guilherme Silveira <guilhermesf1210@gmail.com>
2024-05-29 13:06:53 -0300

    Inserção de dados Endereço

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,15 +5,17 @@
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
 
--- insert into pessoa (id, nome, cpf) values(1, 'Leandra', '111');
--- insert into pessoa (id, nome, cpf) values(2, 'Fredson', '222');
--- insert into pessoa (id, nome, cpf) values(3, 'Marco', '333');
--- insert into pessoa (id, nome, cpf) values(4, 'Carlos Henrique', '444');
-
-
--- insert into estado (nome, sigla) values('Tocantins', 'TO');
--- insert into estado (nome, sigla) values('Goias', 'GO');
-
--- insert into cidade (nome, id_estado) values('Palmas', 1);
--- insert into cidade (nome, id_estado) values('Paraíso', 1);
--- insert into cidade (nome, id_estado) values('Goiânia', 2);
+INSERT INTO endereco(numero, cep, cidade, estado, logradouro, nome, bairro, complemento)
+VALUES 
+    (123, '77000-000', 'Palmas', 'TO', 'Rua das Flores', 'Casa Azul', 'Centro', 'kitnet 4'),
+    (259, '77001-001', 'Gurupi', 'TO', 'Avenida dos Bandeirantes', 'Casa Verde', 'Jardim Bela Vista', 'Esquina com a Rua Principal'),
+    (958, '77002-002', 'Araguaína', 'TO', 'Travessa das Oliveiras', 'Casa Amarela', 'Setor Norte', 'Ao lado da praça'),
+    (202, '77003-003', 'Porto Nacional', 'TO', 'Rua das Águias', 'Casa Vermelha', 'Centro', 'Próximo à Escola Municipal'),
+    (144, '77004-004', 'Paraíso do Tocantins', 'TO', 'Avenida das Rosas', 'Casa Rosa', 'Jardim Primavera', 'Próximo ao Supermercado'),
+    (303, '77005-005', 'Colinas do Tocantins', 'TO', 'Alameda dos Ipês', 'Casa Lilás', 'Centro', 'Ao lado do Banco do Brasil'),
+    (125, '77006-006', 'Palmas', 'TO', 'Rua das Flores', 'Casa Azul', 'Centro', 'kitnet 4'),
+    (457, '77007-007', 'Gurupi', 'TO', 'Avenida dos Bandeirantes', 'Casa Verde', 'Jardim Bela Vista', 'Esquina com a Rua Principal'),
+    (557, '77008-008', 'Araguaína', 'TO', 'Travessa das Oliveiras', 'Casa Amarela', 'Setor Norte', 'Ao lado da praça'),
+    (106, '77009-009', 'Porto Nacional', 'TO', 'Rua das Águias', 'Casa Vermelha', 'Centro', 'Próximo à Escola Municipal'),
+    (720, '77010-010', 'Paraíso do Tocantins', 'TO', 'Avenida das Rosas', 'Casa Rosa', 'Jardim Primavera', 'Próximo ao Supermercado'),
+    (408, '77011-011', 'Colinas do Tocantins', 'TO', 'Alameda dos Ipês', 'Casa Lilás', 'Centro', 'Ao lado do Banco do Brasil');
